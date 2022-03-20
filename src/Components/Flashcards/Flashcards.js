389ce32049a71665ece48cc47eaa68716e94d218
santannaizadora <santannaizadora@gmail.com>
2022-03-20 17:09:11 -0300

    feat: layout without icons & buttons

--- src/Components/Flashcards/Flashcards.js
+++ src/Components/Flashcards/Flashcards.js
@@ -1,9 +1,9 @@
 import './flashcards.css'
 import { decks } from '../../utils/decks.js'
-import { useState } from 'react';
-import turn from '../../assets/img/setinha.png'
+//import { useState } from 'react';
 import Card from '../Card/Card';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 export default function Flashcards(props) {
     const { setStartRecall, deckSelected, goal } = props;
@@ -17,13 +17,14 @@ export default function Flashcards(props) {
 
     return (
         <>
-        <Header/>
-        <div className='flashcards'>
-            {shuffledCards.map((card, index) => {
-                const { question, answer } = card;
-                return <Card key={index} question={question} answer={answer} numQuestion={index + 1} />
-            })}
-        </div>
+            <Header />
+            <div className='flashcards'>
+                {shuffledCards.map((card, index) => {
+                    const { question, answer } = card;
+                    return <Card key={index} question={question} answer={answer} numQuestion={index + 1} />
+                })}
+            </div>
+            <Footer />
         </>
     )
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+export default function Footer() {
+    return (
+
+        <footer>
+            <p>0/0 CONCLUÍDOS</p>
+        </footer>
+
+    )
+}
