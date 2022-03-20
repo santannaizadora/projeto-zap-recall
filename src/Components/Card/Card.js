0bc4e0cdb7f2816d0ac5d3b38fcba618f67c3b0a
santannaizadora <santannaizadora@gmail.com>
2022-03-20 20:00:33 -0300

    feat: handle answereds questions

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,6 @@
 import turn from '../../assets/img/setinha.png'
 import { useState } from 'react';
-import {icons} from '../../utils/icons'
+import { icons } from '../../utils/icons'
 import './card.css'
 
 
@@ -8,62 +8,80 @@ const Card = (props) => {
     const [cardSelected, setCardSelected] = useState(false);
     const [turnedCard, setTurnedCard] = useState(false);
     const [isAnsweredCard, setIsAnsweredCard] = useState(false);
-    const { question, answer, numQuestion, setAnsweredQuestions, answeredQuestions, setFooterIcons ,footerIcons } = props
+    const [questionIcon, setQuestionIcon] = useState(icons[0])
+    const [cssColor, setCssColor] = useState(false);
+
+    const { question, answer, numQuestion, setAnsweredQuestions, answeredQuestions, setFooterIcons, footerIcons } = props
 
     const cssMaxCard = `card ${cardSelected ? "" : "hide"}`;
     const cssMinCard = `question ${(cardSelected || turnedCard) ? "hide" : ""}`;
     const cssTurnCard = `${(turnedCard) ? "rotate" : ""}`
-    let newIcon = ''
 
-    const questionIcon = `${(isAnsweredCard) ? newIcon : icons[0]}`
+    const handleClick = (index) => {
+        setAnsweredQuestions(answeredQuestions + 1);
+        setFooterIcons([...footerIcons, icons[index]])
+        setIsAnsweredCard(true)
+        setQuestionIcon(icons[index])
+        let color = ''
+        if (index === 1) {
+            color = 'red-text'
+        } else if (index === 2) {
+            color = 'yellow-text'
+        } else {
+            color = 'green-text'
+        }
+
+        setCssColor(color)
+    }
+
     return (
         <div className='flashcard'>
-            <div className={cssMinCard} onClick={() => {
-                !isAnsweredCard ? setCardSelected(true):setCardSelected(false)
-            }}>
-                <h1>Pergunta {numQuestion}</h1>
-                <ion-icon name={questionIcon}></ion-icon>
-            </div>
-            <div className={cssMaxCard + cssTurnCard}>
-                <div className=" front-face face">
+            {
+                isAnsweredCard
+                    ?
+                    <>
+                        <div className={`question ${cssColor}`}>
+                            <h1 className='answered'>Pergunta {numQuestion}</h1>
+                            <ion-icon name={questionIcon}></ion-icon>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div className={cssMinCard} onClick={() => {
+                            !isAnsweredCard ? setCardSelected(true) : setCardSelected(false)
+                        }}>
+                            <h1>Pergunta {numQuestion}</h1>
+                            <ion-icon name={questionIcon}></ion-icon>
+                        </div>
+                        <div className={cssMaxCard + cssTurnCard}>
+                            <div className=" front-face face">
 
-                    <p className='card-text'>{answer}</p>
-                    <div className='buttons'>
-                        <button
-                            className='red'
-                            onClick={() => {
-                                setAnsweredQuestions(answeredQuestions + 1);
-                                newIcon = icons[1]
-                                setFooterIcons([...footerIcons, icons[1]])
-                                setIsAnsweredCard(true)
-                            }}
-                        >Não lembrei</button>
-                        <button className='yellow'
-                            onClick={() => {
-                                setAnsweredQuestions(answeredQuestions + 1);
-                                newIcon = icons[2]
-                                setFooterIcons([...footerIcons, icons[2]])
-                            }}
-                        >Quase lembrei</button>
-                        <button className='green'
-                            onClick={() => {
-                                setAnsweredQuestions(answeredQuestions + 1);
-                                newIcon = icons[3]
-                                setFooterIcons([...footerIcons, icons[3]])
-                            }}
-                        >Zap!</button>
-                    </div>
+                                <p className='card-text'>{answer}</p>
+                                <div className='buttons'>
+                                    <button
+                                        className='red'
+                                        onClick={() => handleClick(1)}
+                                    >Não lembrei</button>
+                                    <button className='yellow'
+                                        onClick={() => handleClick(2)}
+                                    >Quase lembrei</button>
+                                    <button className='green'
+                                        onClick={() => handleClick(3)}
+                                    >Zap!</button>
+                                </div>
 
-                </div>
-                <div className='back-face face'>
-                    <p className='card-text'>{question}</p>
-                    <img
-                        className='arrow'
-                        src={turn} alt='virar card'
-                        onClick={() => setTurnedCard(true)}
-                    />
-                </div>
-            </div>
+                            </div>
+                            <div className='back-face face'>
+                                <p className='card-text'>{question}</p>
+                                <img
+                                    className='arrow'
+                                    src={turn} alt='virar card'
+                                    onClick={() => setTurnedCard(true)}
+                                />
+                            </div>
+                        </div>
+                    </>
+            }
         </div>
     )
 
